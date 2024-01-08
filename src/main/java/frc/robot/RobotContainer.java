ba162ff595f31d4db7ed18dc116248d90ec1b99b
BCR <frc294@gmail.com>
2024-01-07 16:34:35 -0800

    Added controls, preferences, and sim files

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,7 +51,7 @@ import frc.robot.utilities.TrajectoryCache.TrajectoryFacing;
  */
 public class RobotContainer {
   // Define robot key utilities (DO THIS FIRST)
-  private final FileLog log = new FileLog("H5");
+  private final FileLog log = new FileLog("J1");
   private final AllianceSelection allianceSelection = new AllianceSelection(log);
   private final Compressor compressor = new Compressor(PneumaticsModuleType.REVPH);
   private final Field field = new Field(allianceSelection, log);
